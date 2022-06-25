793616b4bd5b6e54b96d5c08d6822d5e43babff0
John Ruddock <john.jxr@pm.me>
2022-06-25 13:51:41 -0400

    local testing env set

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,33 @@
-// Write your tests here
+const db = require('../data/dbConfig');
+const server = require('./server');
+const request = require('supertest');
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+})
+
+test('check env var', async () => {
+  expect(process.env.NODE_ENV).toBe('testing');
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+
+beforeEach(async () => {
+  await db('users').truncate();
+})
+
+afterAll(async () => {
+  await db.destroy();
+})
+
+describe('HTTP tests', () => {
+  test('API is running', async () => {
+    let res = await request(server).get('/');
+    expect(res).toBeDefined();
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ api: 'up' });
+  })
 })
